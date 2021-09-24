37c35ee96b57434f1b9eb29797e35b9174f59144
zh6 <1144941477@qq.com>
2021-09-24 15:31:03 +0800

    add

---
+++ api/v1/sys_module.py
@@ -0,0 +1,28 @@
+from typing import Any
+from datetime import timedelta
+
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+
+from core import security
+from common import deps, logger
+from core.config import settings
+from schemas import sys_user,response_code
+from service.sys_user import curd_user
+
+router = APIRouter()
+
+@router.post("/user/info", summary="获取用户信息", name="获取用户信息", description="此API没有验证权限")
+async def get_user_info(
+        *,
+        current_user: sys_user.UserBase = Depends(deps.get_current_user)
+) -> Any:
+    """
+    获取用户信息 这个路由分组没有验证权限
+    :param current_user:
+    :return:
+    """
+    return response_code.resp_200(data={
+        "nickname": current_user.nickname,
+        "avatar": current_user.avatar
+    })
